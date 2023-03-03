0cc3f07aff24363c4feffdc1c050b1b970702e80
alextrevgoda <83908107+alextrevgoda@users.noreply.github.com>
2023-03-03 15:18:25 +0200

    fix: fix deployment

--- .github/actions/createDeployment.js
+++ .github/actions/createDeployment.js
@@ -2,7 +2,7 @@ module.exports = async ({ github, context }) => {
   await github.rest.repos.createDeployment({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    ref: context.ref,
+    ref: process.env.GITHUB_SHA,
     environment: process.env.environment,
   });
 };
